efffb2f702202f0abcfa91808b057255d83da7cb
Angela Lingan Tan <angelalingan@gmail.com>
2022-08-05 01:26:41 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,3 @@ alert('Hello world');
 
 let favoriteFood= 'Tteokbokki';
 document.write(favoriteFood);
-
-//Using HTML, CSS, and JavaScript, this is a web Pokédex application that displays a list of Pokémon with their details.//
