124b02b760203fd37f316da5effe0283a14cf424
Angela Lingan Tan <angelalingan@gmail.com>
2022-08-07 02:51:10 -0500

    Removed all existing code from previous task and created new pokemonList variable
    Objects added to array, each object representing one Pokémon. 4 keys added to each Pokémon object with value for each item.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-alert('Hello world');
+let pokemonList = [
+	{	
+		name: 'Bulbasaur',
+		height: 0.7,
+		weight: 6.9,
+		types: ['grass', 'poison']
+	},
 
-let favoriteFood= 'Tteokbokki';
-document.write(favoriteFood);
+	{
+		name: 'Butterfree',
+		height: 1.1,
+		weight: 32,
+		types: ['bug', 'flying']
+	},
+
+	{
+		name: 'Pidgeotto',
+		height: 1.1,
+		weight: 30,
+		types: ['flying', 'normal']
+	},
+
+	{
+		name: 'Jigglypuff',
+		height: 0.5,
+		weight: 5.5,
+		types: ['fairy', 'normal']
+	},
+];	
